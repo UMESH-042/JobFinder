47686948d5998de4340145aae96ba3312c64fbc3
UMESH-042 <202151042@iiitvadodara.ac.in>
2023-07-02 21:32:16 +0530

    Modified DisplayJobs

--- lib/view/DisplayJobs.dart
+++ lib/view/DisplayJobs.dart
@@ -22,85 +22,204 @@ class JobListPage extends StatelessWidget {
 
           final jobDocs = snapshot.data!.docs;
 
-          return ListView.builder(
-            itemCount: jobDocs.length,
-            itemBuilder: (context, index) {
-              final jobSnapshot = jobDocs[index];
-              final jobData = jobSnapshot.data() as Map<String, dynamic>?;
+          final popularJobs = jobDocs.take(3).toList(); // Get the first 3 jobs as popular jobs
+          final recentPosts = jobDocs.skip(3).toList(); // Skip the first 3 jobs for recent posts
 
-              if (jobData == null) {
-                // Handle null data
-                return SizedBox.shrink();
-              }
-
-              final location = jobData['location'] ?? 'Unknown';
-              final salary = jobData['salary'] ?? 'Unknown';
-              final category = jobData['category'] ?? 'Unknown';
-              final image = jobData['image'];
-              final jobtype = jobData['jobtype'];
-
-              return Card(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+          return ListView(
+            padding: EdgeInsets.all(16),
+            children: [
+              Text(
+                'Popular Jobs',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(16),
-                  leading: image != null
-                      ? ClipRRect(
+              ),
+              SizedBox(height: 16),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: popularJobs.length,
+                  itemBuilder: (context, index) {
+                    final jobSnapshot = popularJobs[index];
+                    final jobData = jobSnapshot.data() as Map<String, dynamic>?;
+
+                    if (jobData == null) {
+                      // Handle null data
+                      return SizedBox.shrink();
+                    }
+
+                    final location = jobData['location'] ?? 'Unknown';
+                    final salary = jobData['salary'] ?? 'Unknown';
+                    final category = jobData['category'] ?? 'Unknown';
+                    final image = jobData['image'];
+                    final jobtype = jobData['jobtype'];
+
+                    return Container(
+                      width: 250,
+                      margin: EdgeInsets.only(right: 16),
+                      child: Card(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            image,
-                            fit: BoxFit.cover,
-                            width: 60,
-                            height: 60,
-                          ),
-                        )
-                      : Icon(Icons.image,
-                          size: 60), // Placeholder if image is not available
-                  title: Text(
-                    category,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 8),
-                      Text(jobtype,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey[600],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(8),
+                                ),
+                                child: Container(
+                                 margin: EdgeInsets.all(10),
+                                  child: Image.network(
+                                    image ?? '',
+                                    fit: BoxFit.cover,
+                                    // width: 70,
+                                    // height: 100,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    category,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    jobtype,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Location: $location',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Salary: \$${salary}/m',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(height: 4),
-                      // Text(
-                      //   '\$$salary/m',
-                      //   style: TextStyle(
-                      //     fontSize: 14,
-                      //     color: Colors.grey[600],
-                      //   ),
-                      // ),
-                      
-                    ],
-                  
-                  ),
-                  trailing: Text(
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 32),
+              Text(
+                'Recent Posts',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: recentPosts.length,
+                itemBuilder: (context, index) {
+                  final jobSnapshot = recentPosts[index];
+                  final jobData = jobSnapshot.data() as Map<String, dynamic>?;
+
+                  if (jobData == null) {
+                    // Handle null data
+                    return SizedBox.shrink();
+                  }
+
+                  final location = jobData['location'] ?? 'Unknown';
+                  final salary = jobData['salary'] ?? 'Unknown';
+                  final category = jobData['category'] ?? 'Unknown';
+                  final image = jobData['image'];
+                  final jobtype = jobData['jobtype'];
+
+                  return Card(
+                    margin: EdgeInsets.only(bottom: 16),
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ListTile(
+                      contentPadding: EdgeInsets.all(16),
+                      leading: image != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(
+                                image,
+                                fit: BoxFit.cover,
+                                width: 60,
+                                height: 60,
+                              ),
+                            )
+                          : Icon(Icons.image,
+                              size: 60), // Placeholder if image is not available
+                      title: Text(
+                        category,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 8),
+                          Text(
+                            jobtype,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          // Text(
+                          //   '\$$salary/m',
+                          //   style: TextStyle(
+                          //     fontSize: 14,
+                          //     color: Colors.grey[600],
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                      trailing: Text(
                         '\$$salary/m',
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.grey[600],
                         ),
                       ),
-                  onTap: () {
-                    // Handle onTap event
-                  },
-                ),
-              );
-            },
+                      onTap: () {
+                        // Handle onTap event
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
           );
         },
       ),
