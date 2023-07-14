30b47ca8d4d7cf386d381cd98622c6676ab28a21
UMESH-042 <202151042@iiitvadodara.ac.in>
2023-07-15 04:00:29 +0530

    Optimized the Page Little bit

--- lib/view/Profile_Screen.dart
+++ lib/view/Profile_Screen.dart
@@ -1,74 +1,127 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
+
+// import 'Home_Screen.dart';
 
 // class ProfileScreen extends StatefulWidget {
 //   final String useremail;
-//   const ProfileScreen({super.key, required this.useremail});
+
+//   const ProfileScreen({Key? key, required this.useremail}) : super(key: key);
 
 //   @override
 //   State<ProfileScreen> createState() => _ProfileScreenState();
 // }
 
 // class _ProfileScreenState extends State<ProfileScreen> {
+//   bool isLoading = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     Future.delayed(Duration(seconds: 2), () {
+//       setState(() {
+//         isLoading = false;
+//       });
+//     });
+//   }
+
 //   @override
 //   Widget build(BuildContext context) {
+//     print(widget.useremail);
 //     return Scaffold(
 //       appBar: AppBar(
+//         backgroundColor: Color.fromARGB(255, 76, 175, 142),
 //         title: Text('User Profile'),
 //       ),
-//       body: FutureBuilder<DocumentSnapshot>(
-//         future:
-//             FirebaseFirestore.instance.collection('users').doc(widget.useremail).get(),
-//         builder:
-//             (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return Center(child: CircularProgressIndicator());
-//           }
-//           if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           }
-//           if (!snapshot.hasData) {
-//             return Center(child: Text('No data available'));
-//           }
+//       body: isLoading
+//           ? ShimmerEffect()
+//           : FutureBuilder<DocumentSnapshot>(
+//               future: FirebaseFirestore.instance
+//                   .collection('users')
+//                   .doc(widget.useremail)
+//                   .get(),
+//               builder: (BuildContext context,
+//                   AsyncSnapshot<DocumentSnapshot> snapshot) {
+//                 if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return Center(child: CircularProgressIndicator());
+//                 }
+//                 if (snapshot.hasError) {
+//                   return Center(child: Text('Error: ${snapshot.error}'));
+//                 }
+//                 if (!snapshot.hasData) {
+//                   return Center(child: Text('No data available'));
+//                 }
 
-//           // User data is available
-//           final userData = snapshot.data!.data() as Map<String, dynamic>;
+//                 // User data is available
+//                 final userData = snapshot.data!.data() as Map<String, dynamic>;
 
-//           return Padding(
-//             padding: EdgeInsets.all(16.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 CircleAvatar(
-//                   radius: 80,
-//                   backgroundImage: NetworkImage(userData['imageUrl']),
-//                 ),
-//                 SizedBox(height: 16.0),
-//                 Text(
-//                   userData['name'],
-//                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-//                 ),
-//                 SizedBox(height: 8.0),
-//                 Text(
-//                   userData['bio'],
-//                   style: TextStyle(fontSize: 16),
-//                   textAlign: TextAlign.center,
-//                 ),
-//                 SizedBox(height: 8.0),
-//                 Text(
-//                   userData['email'],
-//                   style: TextStyle(fontSize: 16),
-//                 ),
-//               ],
+//                 return Padding(
+//                   padding: EdgeInsets.all(16.0),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: [
+//                       Stack(
+//                         children: [
+//                           Container(
+//                             width: 160,
+//                             height: 160,
+//                             decoration: BoxDecoration(
+//                               shape: BoxShape.circle,
+//                               image: DecorationImage(
+//                                 image: NetworkImage(userData['imageUrl']),
+//                                 fit: BoxFit.cover,
+//                               ),
+//                             ),
+//                           ),
+//                           Positioned(
+//                             bottom: 0,
+//                             right: 4,
+//                             child: Container(
+//                               width: 40,
+//                               height: 40,
+//                               decoration: BoxDecoration(
+//                                 shape: BoxShape.circle,
+//                                 color: Colors.white,
+//                               ),
+//                               child: Icon(
+//                                 Icons.camera_alt,
+//                                 color: Colors.grey[600],
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       SizedBox(height: 16.0),
+//                       Text(
+//                         userData['name'],
+//                         style: TextStyle(
+//                             fontSize: 24, fontWeight: FontWeight.bold),
+//                       ),
+//                       SizedBox(height: 8.0),
+//                       Text(
+//                         userData['bio'],
+//                         style: TextStyle(fontSize: 16),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 16.0),
+//                       Divider(),
+//                       ListTile(
+//                         leading: Icon(Icons.email),
+//                         title: Text(
+//                           userData['email'],
+//                           style: TextStyle(fontSize: 16),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 );
+//               },
 //             ),
-//           );
-//         },
-//       ),
 //     );
 //   }
 // }
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -84,110 +137,121 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool isLoading = true;
+  late Future<DocumentSnapshot> _userDataFuture;
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2), () {
-      setState(() {
-        isLoading = false;
-      });
-    });
+    _userDataFuture = getUserData();
+  }
+
+  Future<DocumentSnapshot> getUserData() async {
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.useremail)
+        .get();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(widget.useremail);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 76, 175, 142),
         title: Text('User Profile'),
       ),
-      body: isLoading
-          ? ShimmerEffect()
-          : FutureBuilder<DocumentSnapshot>(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(widget.useremail)
-                  .get(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<DocumentSnapshot> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                }
-                if (!snapshot.hasData) {
-                  return Center(child: Text('No data available'));
-                }
+      body: FutureBuilder<DocumentSnapshot>(
+        future: _userDataFuture,
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData) {
+            return Center(child: Text('No data available'));
+          }
 
-                // User data is available
-                final userData = snapshot.data!.data() as Map<String, dynamic>;
+          final userData = snapshot.data!.data() as Map<String, dynamic>;
 
-                return Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Stack(
-                        children: [
-                          Container(
-                            width: 160,
-                            height: 160,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                image: NetworkImage(userData['imageUrl']),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 4,
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.white,
-                              ),
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.grey[600],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16.0),
-                      Text(
-                        userData['name'],
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8.0),
-                      Text(
-                        userData['bio'],
-                        style: TextStyle(fontSize: 16),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 16.0),
-                      Divider(),
-                      ListTile(
-                        leading: Icon(Icons.email),
-                        title: Text(
-                          userData['email'],
-                          style: TextStyle(fontSize: 16),
+          return Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: 160,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: CachedNetworkImageProvider(userData['imageUrl']),
+                          fit: BoxFit.cover,
                         ),
                       ),
-                    ],
+                      child: ClipOval(
+                        child: CachedNetworkImage(
+                          imageUrl: userData['imageUrl'],
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => PlaceholderImage(),
+                          errorWidget: (context, url, error) => PlaceholderImage(),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 4,
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  userData['name'],
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  userData['bio'],
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 16.0),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text(
+                    userData['email'],
+                    style: TextStyle(fontSize: 16),
                   ),
-                );
-              },
+                ),
+              ],
             ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class PlaceholderImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
     );
   }
 }
