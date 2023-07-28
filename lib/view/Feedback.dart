b8ffa30e5085b04145c32625663819719dde89f6
UMESH-042 <202151042@iiitvadodara.ac.in>
2023-07-29 01:02:45 +0530

    Added function for adding feedback in firebase

--- lib/view/Feedback.dart
+++ lib/view/Feedback.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class FeedbackPage extends StatefulWidget {
@@ -17,14 +19,41 @@ class _FeedbackPageState extends State<FeedbackPage> {
 
   TextEditingController _feedbackController = TextEditingController();
 
-  void _submitFeedback() {
+  Future<void> _submitFeedback() async {
     String feedbackText = _feedbackController.text;
-    // For demonstration purposes, we are just printing the feedback and selected types here.
-    print('Feedback submitted: $feedbackText');
-    print('Selected Types: $selectedFeedbackTypes');
-    // You can clear the text field and selected types after submission.
-    _feedbackController.clear();
-    setState(() => selectedFeedbackTypes.clear());
+
+    // Get the email of the current user
+    String? currentUserEmail = FirebaseAuth.instance.currentUser?.email;
+
+    if (currentUserEmail != null) {
+      // Store the feedback data in Firestore
+      await FirebaseFirestore.instance.collection('feedback').add({
+        'email': currentUserEmail,
+        'feedbackText': feedbackText,
+        'selectedFeedbackTypes': selectedFeedbackTypes,
+        'timestamp': FieldValue.serverTimestamp(), // Add a timestamp for sorting purposes
+      });
+
+      // For demonstration purposes, we are just printing the feedback and selected types here.
+      print('Feedback submitted: $feedbackText');
+      print('Selected Types: $selectedFeedbackTypes');
+
+      // You can clear the text field and selected types after submission.
+      _feedbackController.clear();
+      setState(() => selectedFeedbackTypes.clear());
+
+      // Show a SnackBar to thank the user for their feedback
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Thank you for your feedback!'),
+          duration: Duration(seconds: 2), // You can adjust the duration as needed
+        ),
+      );
+
+      //  Future.delayed(Duration(seconds: 1), () {
+      Navigator.pop(context);
+      // });
+    }
   }
 
   Widget feedbackTypeChoiceChip(String label) {
@@ -38,7 +67,8 @@ class _FeedbackPageState extends State<FeedbackPage> {
         ),
       ),
       selected: isSelected,
-      selectedColor:  Color.fromARGB(255, 76, 175, 142), // Custom color for selected chips
+      selectedColor:
+          Color.fromARGB(255, 76, 175, 142), // Custom color for selected chips
       onSelected: (selected) {
         setState(() {
           if (selected) {
@@ -57,13 +87,18 @@ class _FeedbackPageState extends State<FeedbackPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Get the screen size
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:  Color.fromARGB(255, 76, 175, 142),
+        backgroundColor: Color.fromARGB(255, 76, 175, 142),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // This automatically handles navigation to the previous page
+            Navigator.pop(
+                context); // This automatically handles navigation to the previous page
           },
         ),
         title: Text('Feedback Page'),
@@ -77,20 +112,20 @@ class _FeedbackPageState extends State<FeedbackPage> {
               'Please select the type of feedback:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: screenHeight * 0.02), // Adjust spacing based on screen height
             Wrap(
-              spacing: 8,
-              runSpacing: 8,
+              spacing: screenWidth * 0.04, // Adjust spacing based on screen width
+              runSpacing: screenHeight * 0.02, // Adjust spacing based on screen height
               children: feedbackTypes.map((type) {
                 return feedbackTypeChoiceChip(type);
               }).toList(),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: screenHeight * 0.03), // Adjust spacing based on screen height
             Text(
               'Please provide your detailed feedback:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: screenHeight * 0.02), // Adjust spacing based on screen height
             Expanded(
               child: TextFormField(
                 controller: _feedbackController,
@@ -101,10 +136,11 @@ class _FeedbackPageState extends State<FeedbackPage> {
                 ),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: screenHeight * 0.02), // Adjust spacing based on screen height
             ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(255, 76, 175, 142), // Custom color for the button
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(
+                    255, 76, 175, 142), // Custom color for the button
                 // Custom text color for the button
               ),
               onPressed: _submitFeedback,
@@ -116,4 +152,3 @@ class _FeedbackPageState extends State<FeedbackPage> {
     );
   }
 }
-
